752ef5f3288a83fb30299c8f6a2fc18b70eb37e9
Devi Jetta <jettadevi289@gmail.com>
2026-03-10 21:44:25 +0530

    update

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,12 +1,35 @@
-import { useState } from "react"
-function Content(){
-    const [count , setCount] = useState(0) ; 
-    return  (
-        <div>
-            <h2>Products Page </h2>
-
-        </div>
-    
-)
+import { useState, useEffect } from "react";
+import axios from "axios";
+function Content() {
+  const [count, setCount] = useState(0);
+  const [products, setProducts] = useState([]);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+  };
+  const fetchProducts = async () => {
+    const url = "http://localhost:5000/products";
+    const res = await axios.get(url);
+    setProducts(res.data);
+  };
+  useEffect(() => {
+     fetchProducts();
+  }, []);
+  return (
+    <div>
+      <h3>Products Page</h3>
+      <button onClick={decrement}>-</button>
+      {count}
+      <button onClick={increment}>+</button>
+      <hr />
+      <ol>
+        {products.map((product) => (
+          <li>{product.name}</li>
+        ))}
+      </ol>
+    </div>
+  );
 }
-export default Content
+export default Content;
